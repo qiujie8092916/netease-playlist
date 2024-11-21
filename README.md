8198e14d29c520c35fab3e58235554864774ebb3
Jie.Q <qiujieee_empty@outlook.com>
2024-11-22 00:40:34 +0800

    docker

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 通过网易云音乐歌单 ID，生成 [xiaomusic](https://github.com/hanxi/xiaomusic") 的在线歌单地址
 
+### docker 配置
+- /xxx/.env:/usr/src/app/.env  
+- /xxx/music_list:/usr/src/app/music_list
+
+
 ### 环境变量
 | 变量名   | 必须 | 描述   |  
 |--------|------|--------|  
 | CRON   | F   | 循环的 CRON。默认：0 0 * * * |  
-| PLAYLIST_ID   | T   | 网易云音乐歌单 ID   |  
+| PLAYLIST_ID   | T   | 网易云音乐歌单 ID   |
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const cron = require('node-cron');
+const path = require('path');
 
-const cron_expression = process.env.CRON || '0 0 * * *';
-const playlistID = process.env.PLAYLIST_ID;
+require('dotenv').config();
 
+const cronExpression = process.env.CRON || '0 0 * * *';
+const playlistID = process.env.PLAYLIST_ID || '';
+const exportPath = '/usr/src/app/music_list'
 const main = () => {
+  if(!playlistID) {
+    throw `Can not get playlistID of ${playlistID}`
+  }
+
   fetch(`https://music.163.com/api/playlist/detail?id=${playlistID}`, {
     method: 'GET',
   }).then(async response => {
@@ -24,14 +31,26 @@ const main = () => {
       }
     ];
 
-    try {
-      fs.writeFileSync('/usr/src/app/xiaomusic_music_list.json', JSON.stringify(xiaomusic_music_list_json), 'utf-8');
-      console.log('write file sucsess!');
-    } catch (err) {
-        console.error('write file failed: ', err);
-    }
+    if (fs.existsSync(path)) {
+        console.log(`folder 【${exportPath}】 exsit`);
+    } else {
+      console.log(`folder 【${exportPath}】 not exsit, creating`);
 
+      try {
+        fs.mkdirSync(exportPath, { recursive: true });
+        console.log(`folder 【${exportPath}】 create success`);
+      } catch (err) {
+        throw `create folder failed:, ${err}`
+      }
+
+      try {
+        fs.writeFileSync(path.join(exportPath, 'xiaomusic_music_list.json'), JSON.stringify(xiaomusic_music_list_json), 'utf-8');
+        console.log('write file sucsess!');
+      } catch (err) {
+          console.error('write file failed: ', err);
+      }
+    }
   });
 }
 
-cron.schedule(cron_expression, main);
+cron.schedule(cronExpression, main);
