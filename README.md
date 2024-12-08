52b28daeb12c4a271cbc066f8346629bb1d70c58
Jie.Q <qiujieee_empty@outlook.com>
2024-12-08 16:52:37 +0800

    :sparkles: download songs to local folder

--- README.md
+++ README.md
@@ -12,20 +12,31 @@ qiujie8092916/netease-playlist
 - /xxx/music_list:/usr/src/app/music_list
 
 
+### TIPS
+- 歌单方式更新时采用全量覆盖（自用，没必要采用增量覆盖模式）
+- 新增本地音乐方式，历史阶段发现：
+  - 使用 [获取音乐 url - 新版](https://neteasecloudmusicapi.ivelly.com/docs/#/?id=%e8%8e%b7%e5%8f%96%e9%9f%b3%e4%b9%90-url-%e6%96%b0%e7%89%88) 歌曲链接方式，链接有有效期，如果作为音乐的在线播放地址，播放音乐的时候链接可能失效
+  - 使用 `https://music.163.com/song/media/outer/ur` 外链的方式([获取网易云音乐永久外链](https://pelom.cn/archives/107/))，有些音乐的地址可能是 404，所以采用 local 的方式
+  - 目前同时保留歌单方式，依然采用 `https://music.163.com/song/media/outer/ur` 外链。并提供 `METHOD` 的环境变量以支持两种方式
+
+
 ### 环境变量
-| 变量名         | 必须 | 描述                                                |  
-|-------------|----|---------------------------------------------------|  
-| CRON        | F  | 循环的 CRON。默认：0 0 * * *                             |  
-| PLAYLIST_ID | T  | 网易云音乐歌单 ID                                        |
-| MUSIC_U     | T  | 网易云音乐的 web cookie，有效期有一年多，没有 cookie，可能不能获取歌曲的播放地址 |
-| IMMEDIATELY | F  | 是否立即执行。默认：false                                   |
-| MODE        | F  | 全文覆盖还是增量覆盖。默认：'increment', 参考值：all、increment                        |
+| 变量名         | 必须 | 描述                                                                                                                            |  
+|-------------|----|-------------------------------------------------------------------------------------------------------------------------------|  
+| CRON        | F  | 循环的 CRON。默认：0 0 * * *                                                                                                         |  
+| PLAYLIST_ID | T  | 网易云音乐歌单 ID                                                                                                                    |
+| MUSIC_U     | T  | 网易云音乐的 web cookie，有效期有一年多，没有 cookie，可能不能获取歌曲的播放地址                                                                             |
+| IMMEDIATELY | F  | 是否立即执行。默认：false                                                                                                               |
+| METHOD      | F  | 默认: ["radio", "local"], 歌单方式、本地音乐方式                                                                                           |
+| OUTDIR      | F  | 若 `METHOD` 包含 'local'，则需要传入下载输出的路径，此路径可以挂载到 [xiaomusic](https://github.com/hanxi/xiaomusic) 的 `XIAOMUSIC_DOWNLOAD_PATH` 对应的路径 |
 
 
 ### 自行探索
 1. 通过本地起 NG 的方式，获取歌单链接，可不用将歌单链接暴露在公网
 2. 也可将歌单链接挂在 CDN 上，参考：[已支持配置自定义网络歌单，在这里分享你的歌单](https://github.com/hanxi/xiaomusic/issues/78)
 
+
 ### 鸣谢
 - [xiaomusic](https://github.com/hanxi/xiaomusic)
 - [NeteaseCloudMusicApi](https://github.com/Binaryify/NeteaseCloudMusicApi)
+- [netease-music-downloader](https://github.com/Gaohaoyang/netease-music-downloader)
--- index.js
+++ index.js
@@ -1,30 +1,143 @@
 const fs = require('fs');
 const _ = require('lodash')
+const axios = require('axios').default;
 const cron = require('node-cron');
 const path = require('path');
-const { playlist_detail, song_url_v1, song_detail} = require('NeteaseCloudMusicApi')
+const { playlist_detail, song_download_url, song_detail} = require('NeteaseCloudMusicApi')
+const { SingleBar } = require('cli-progress');
 
 require('dotenv').config();
 
-let old_data;
-let new_data;
+// 已存在的电台歌单
+let old_radio_data;
+// 新生成的电台歌单
+let new_radio_data;
+// 下载到本地音乐
+/**
+ * 记录已成功下载的音乐, 定义为 object[]，增加可扩展性
+ * {
+ *  id: number; // 歌曲的 id
+ * }[]
+ * 下个定时任务刷新歌单时，更新此变量，并遍历已下载的音乐取交集：
+ *    新增的歌曲（之前没下载成功的歌曲），（再次）下载
+ *    之前下载了的歌曲，不做任何才做
+ *    删除的歌曲，直接删除本地音乐文件
+ * 下个定时任务刷新歌单时，以上述逻辑来更新本地下载的音乐，以达到本地下载的音乐与网易云歌单的音乐保持同步，同步时间即为 CRON 的配置
+ */
+let old_local_data;
+let new_local_data;
 
 const cookie = `MUSIC_U=${process.env.MUSIC_U}`;
 const playlistID = process.env.PLAYLIST_ID;
-const mode = process.env.MODE ?? 'increment';
+const methodString = process.env.METHOD ?? '["radio", "local"]';
 const cronExpression = process.env.CRON || '0 0 * * *';
 const immediately = process.env.IMMEDIATELY || 'false';
+const outdir = process.env.OUTDIR;
 const exportPath = '/usr/src/app/music_list';
+const xiaomusic_music_list = 'xiaomusic_music_list.json';
+const xiaomusic_music_local_list = 'xiaomusic_music_local_list.json';
+
+let method;
 
 const main = async () => {
   if (!playlistID) {
     throw `Can not get playlistID of ${playlistID}`
   }
+
+  try {
+    method = JSON.parse(methodString.replace(/'/g, '"'));
+  } catch (e) {
+    throw `Can not parse method of ${methodString}`
+  }
+
+  if (method.includes('local') && !outdir) {
+    throw `accepted 'local' in 'METHOD', but have no 'OUTDIR'`;
+  }
+
   task();
 }
 
-const overwriteFile = (data) => {
-  fs.writeFileSync(path.join(exportPath, 'xiaomusic_music_list.json'), JSON.stringify(data), 'utf-8');
+const overwriteRadioFile = (data) => {
+  console.log(`[debugger] ready to write file 【${path.join(exportPath, xiaomusic_music_list)}】...`);
+  try {
+    fs.writeFileSync(path.join(exportPath, xiaomusic_music_list), JSON.stringify(data), 'utf-8');
+    console.log(`[debugger] write file 【${path.join(exportPath, xiaomusic_music_list)}】 sucsess`);
+  } catch (err) {
+    console.error(`[debugger] write file 【${path.join(exportPath, xiaomusic_music_list)}】 failed: ${err}`);
+  }
+}
+
+const overwriteLocalFile = (data) => {
+  console.log(`[debugger] ready to write file 【${path.join(exportPath, xiaomusic_music_local_list)}】...`);
+  try {
+    fs.writeFileSync(path.join(exportPath, xiaomusic_music_local_list), JSON.stringify(data), 'utf-8');
+    console.log(`[debugger] write file 【${path.join(exportPath, xiaomusic_music_local_list)}】 sucsess`);
+  } catch (err) {
+    console.error(`[debugger] write file 【${path.join(exportPath, xiaomusic_music_local_list)}】 failed: ${err}`);
+  }
+}
+
+const splitFileNameAndExt = (filePath) => {
+    // 使用 path.basename 获取文件名部分
+    const fileName = path.basename(filePath);
+
+    // 使用 path.extname 获取扩展名部分
+    const ext = path.extname(fileName);
+
+    // 获取不带扩展名的文件名
+    const nameWithoutExt = fileName.slice(0, -ext.length);
+
+    return {
+        name: nameWithoutExt,
+        extension: ext.startsWith('.') ? ext.slice(1) : ext // 去掉前面的点
+    };
+}
+
+const createSingleBar = (options)  => {
+  return new SingleBar({
+    format: '下载进度 |{bar}| {percentage}% || {value}/{total} KB',
+    barCompleteChar: '█',
+    barIncompleteChar: '░',
+    hideCursor: true,
+    ...options
+  });
+}
+
+const download = async ({
+  url,
+  format,
+  id,
+}) => {
+  try {
+    const response = await axios({
+        method: 'get',
+        url,
+        responseType: 'stream'
+      });
+
+    const totalLength = parseInt(response.headers['content-length'], 10);
+    const bar = createSingleBar();
+    bar.start(Math.round(totalLength/1024), 0);
+
+    const writer = fs.createWriteStream(path.join(outdir, `${id}.${format}`));
+    let downloadedBytes = 0;
+
+    response.data.on('data', (chunk) => {
+      downloadedBytes += chunk.length;
+      bar.update(Math.round(downloadedBytes/1024));
+    });
+
+    response.data.pipe(writer);
+
+    await new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
+
+    bar.stop();
+  } catch (e) {
+    await new Promise((resolve, reject) => reject(e));
+  }
 }
 
 const task = async () => {
@@ -41,12 +154,12 @@ const task = async () => {
 
     const trackIds = playlist.trackIds.map(track => track.id);
 
-    console.log(`[debugger] trackIds`, trackIds);
+    console.log(`[debugger] trackIds`, JSON.stringify(trackIds, null, 2));
 
     Promise.all([song_detail({
       ids: trackIds.join(','),
       cookie,
-    })]).then(([songDetailsResponse]) => {
+    })]).then(async ([songDetailsResponse]) => {
       // const songUrlsJson = songUrlsResponse.body;
       const { songs } = songDetailsResponse.body;
 
@@ -56,23 +169,16 @@ const task = async () => {
 
         // if (urlObj && urlObj.url) {
         return acc.concat({
+          id,
           name,
-          url: `https://music.163.com/song/media/outer/url?id=${id}.mp3`
         })
         // }
       }, []);
 
-      new_data = [
-        {
-          name: playlist.name,
-          musics
-        }
-      ];
-
       if (fs.existsSync(path)) {
           console.log(`[debugger] folder 【${exportPath}】 exsit`);
       } else {
-        console.log(`[debugger] folder 【${exportPath}】 not exsit, creating`);
+        console.log(`[debugger] folder 【${exportPath}】 not exsit, creating...`);
 
         try {
           fs.mkdirSync(exportPath, { recursive: true });
@@ -80,32 +186,119 @@ const task = async () => {
         } catch (err) {
           throw `create folder failed:, ${err}`
         }
+      }
 
+      if (method.includes('radio')) {
+        new_radio_data = [
+          {
+            name: playlist.name,
+            musics: musics.map(({ name, id }) => ({
+              name,
+              url: `https://music.163.com/song/media/outer/url?id=${id}.mp3`
+            }))
+          }
+        ];
+
+        overwriteRadioFile(new_radio_data)
+      }
+
+      if (method.includes('local')) {
+        new_local_data = [];
+        if (fs.existsSync(path.join(exportPath, xiaomusic_music_local_list))) {
+          console.log(`[debugger] file 【${path.join(exportPath, xiaomusic_music_local_list)}】 exsit`);
+        } else {
+          console.log(`[debugger] file 【${path.join(exportPath, xiaomusic_music_local_list)}】 not exsit, creating...`);
+
+          try {
+            overwriteLocalFile([]);
+            console.log(`[debugger] file 【${path.join(exportPath, xiaomusic_music_local_list)}】 create success`);
+          } catch (err) {
+            throw `create file 【${path.join(exportPath, xiaomusic_music_local_list)}】 failed:, ${err}`
+          }
+        }
+
+        let dataString;
         try {
-          if (mode === 'increment') {
+          dataString = fs.readFileSync(path.join(exportPath, xiaomusic_music_local_list), 'utf-8');
+          old_local_data = JSON.parse(dataString);
+        } catch (e) {
+          console.error(`parse old_local_data failed: ${e}`);
+          old_local_data = []
+        }
+
+        console.log(`[debugger] musics.length`, musics.length);
+
+        // 更新本地下载的音乐
+        // 新增歌曲（之前未成功下载的歌曲）、已经成功下载的歌曲
+        for (let i = 0; i < musics.length; i++) {
+          const music = musics[i];
+
+          const { id, name } = music;
+          // 查找音乐是否已经成功下载
+          const downloaded = old_local_data.find(it => it.id === id)
+          if (!downloaded) {
+            console.log(`[debugger] [${name}](${id}) is not in local, ready to download...`)
+
+            // 新增歌曲（之前未成功下载的歌曲），下载并记录
             try {
-              const dataString = fs.readFileSync(path.join(exportPath, 'xiaomusic_music_list.json'), 'utf-8');
-              old_data = JSON.parse(dataString);
+              const songDownloadResponse = await song_download_url({
+                id,
+                // level: 'jymaster',
+                cookie,
+              });
 
-              let { musics: new_musics } = new_data[0];
-              const { musics: old_musics } = old_data[0];
+              const { data: {url, type} } = songDownloadResponse.body;
 
-              console.log('[debugger] new_data', JSON.stringify(new_data, null, 2))
-              console.log('[debugger] old_data', JSON.stringify(old_data, null, 2))
+              console.log(`[debugger] [${name}](${id}) get download url success: `, url, type);
 
-              const data = _.unionWith(new_musics, old_musics, (a, b) => a.name === b.name);
+              try {
+                await download({
+                  url,
+                  id,
+                  format: type,
+                })
 
-              overwriteFile(data)
-            } catch (e) {
-              overwriteFile(new_data)
+                console.log(`[debugger] [${name}](${id}) download success`)
+
+                // 下载成功，记录到 new_local_data
+                new_local_data = new_local_data.concat({
+                  id,
+                })
+              } catch (e) {
+                // 下载失败，打印错误，什么都不做，等到下次 cron 触发更新歌单时再尝试重新下载
+                console.error(`[debugger] [${name}](${id}) download failed: ${err.toString()}`)
+              }
+            } catch(e) {
+              console.log(`[debugger] [${name}](${id}) get download url failed: `, e);
             }
           } else {
-            overwriteFile(new_data)
+              console.log(`[debugger] [${downloaded.name}](${downloaded.id}) is in local, do nothing`)
+            // 已经下载过的音乐只做记录，直接跳过
+            new_local_data = new_local_data.concat(downloaded);
           }
-          console.log('[debugger] write file sucsess!');
-        } catch (err) {
-            console.error('[debugger] write file failed: ', err);
         }
+
+        // 歌单里删除了歌曲
+        // 不知道下载的歌曲的后缀名，只能遍历 outdir 下的所有文件，用 id 来匹配，找出文件名
+        const songsFilename = fs.readdirSync(outdir);
+        for (let i = 0; i < old_local_data.length; i++) {
+          const localSongRecord = old_local_data[i];
+          const { id } =  localSongRecord;
+
+          const exist = musics.find(music => music.id === id);
+          if (!exist) {
+            songsFilename.forEach(songFilename => {
+              const { name: filename} = splitFileNameAndExt(songFilename);
+              if (filename === `${id}`) {
+                console.log(`[debugger] ${id} is not in playlist, ready to delete...`)
+                // 歌单里找不到歌曲，说明歌曲已经从歌单里删除，同步删除本地音乐
+                fs.unlink(path.join(outdir, songFilename));
+                console.log(`[debugger] [${songFilename}] delete success`)
+              }
+            })
+          }
+        }
+        overwriteLocalFile(new_local_data);
       }
     })
   }).catch((err) => {
